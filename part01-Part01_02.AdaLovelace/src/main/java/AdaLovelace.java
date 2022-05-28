bf76808e3a2a7f90944df26ec437da905ca0fa47
Tracy Neil Padilla <padillatracyneil@gmail.com>
2022-05-28 09:42:26 +0800

    Add files via upload

---
+++ part01-Part01_02.AdaLovelace/src/main/java/AdaLovelace.java
@@ -0,0 +1,12 @@
+
+public class AdaLovelace {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Ada Lovelace");
+        
+        
+        
+    }
+    
+}
